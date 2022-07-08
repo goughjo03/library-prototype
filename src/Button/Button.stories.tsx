5aae34d6ccae2c6ec77382fd58e68c35e0001677
Joey Gough <j.gough@novafutur.com>
2022-07-08 14:27:28 +0100

    add storybook (local dev)

---
+++ src/Button/Button.stories.tsx
@@ -0,0 +1,27 @@
+// YourComponent.stories.ts|tsx
+
+import React from 'react';
+
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Button from '.';
+
+//👇 This default export determines where your story goes in the story list
+export default {
+  /* 👇 The title prop is optional.
+  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+  * to learn how to generate automatic titles
+  */
+  title: 'Button',
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+//👇 We create a “template” of how args map to rendering
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const BasicButton = Template.bind({});
+
+BasicButton.args = {
+  /*👇 The args you need here will depend on your component */
+  children: 'Hello Button',
+};
---
+++ src/TextInput/TextInput.stories.tsx
@@ -0,0 +1,36 @@
+// YourComponent.stories.ts|tsx
+
+import React from "react";
+
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import TextInput from ".";
+
+//👇 This default export determines where your story goes in the story list
+export default {
+  /* 👇 The title prop is optional.
+   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+   * to learn how to generate automatic titles
+   */
+  title: "Text Input",
+  component: TextInput,
+} as ComponentMeta<typeof TextInput>;
+
+//👇 We create a “template” of how args map to rendering
+const Template: ComponentStory<typeof TextInput> = (args) => (
+  <TextInput {...args} />
+);
+
+export const WithPlaceholder = Template.bind({});
+
+WithPlaceholder.args = {
+  /*👇 The args you need here will depend on your component */
+  placeholder: "This is a plarcholder",
+};
+
+export const WithoutPlaceholder = Template.bind({});
+
+WithoutPlaceholder.args = {
+  /*👇 The args you need here will depend on your component */
+  placeholder: undefined,
+};
